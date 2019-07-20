ff2865aaec5c767d1646742da3439edd6b71423e
github username <barbara.m.washington@gmail.com>
2019-07-19 20:55:20 -0700

    add onClick functions for language selection

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
 class App extends React.Component {
-  render() {
-    return <div className="ui container">App</div>
+  state = { language: 'english' };
+
+  onLanguageChange = language => {
+    this.setState({ language });
   }
+
+  render() {
+    return <div className="ui container">
+      <div>
+        Select a language:
+        <i className="flag us" onClick={() => this.onLanguageChange('english')} />
+        <i className="flag jp" onClick={() => this.onLanguageChange('japanese')} />
+      </div>
+      {this.state.language}
+    </div>
+  };
 }
 
 export default App;
